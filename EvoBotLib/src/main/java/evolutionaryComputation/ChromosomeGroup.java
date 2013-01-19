a853d710cf4c059a2511d21a9521c299ef5e9cea
jose <jose@jose-N55SF>
2013-01-19 15:51:00 +0100

    Añadido Individual.xml a los recursos del proyecto
    Comienzo del desarrollo de la parte interactiva del algoritmo evolutivo.

---
+++ EvoBotLib/src/main/java/evolutionaryComputation/ChromosomeGroup.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evolutionaryComputation;
+
+import java.util.List;
+
+/**
+ *
+ * @author jose
+ */
+public class ChromosomeGroup {
+
+ private String name;
+ 
+ private List<Integer> chromosomeIndexList;
+}
